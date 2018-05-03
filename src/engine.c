24695621b3650129a496859b204720d48d59f672
Matthias Hannig <matthias@hannig.cc>
2018-05-03 19:12:27 +0200

    improved error handling

--- src/engine.c
+++ src/engine.c
@@ -1,11 +1,10 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdarg.h>
 
 #include "engine.h"
 
-void engine_lua_error(lua_State *L, const char *fmt, ...);
+void engine_lua_error(lua_State *L);
 
 
 /*
@@ -29,10 +28,7 @@ einhorn_engine* engine_init(einhorn_config* config)
     // Load program
     if (luaL_loadfile(engine->L, config->program_filename) 
         || lua_pcall(engine->L, 0, 0, 0)) {
-            engine_lua_error(engine->L,
-                             "Error while loading program: %s\n",
-                             lua_tostring(engine->L, -1));
-
+            engine_lua_error(engine->L);
         return NULL;
     }
 
@@ -51,13 +47,13 @@ int engine_run(einhorn_engine* engine)
 /*
  * Helper: Print lua error
  */
-void engine_lua_error(lua_State *L, const char *fmt, ...)
+void engine_lua_error(lua_State *L)
 {
-    va_list args;
-    va_start(args, fmt);
+    const char* error = lua_tostring(L, -1);
 
-    vfprintf(stderr, fmt, args);
-    va_end(args);
+    fprintf(stderr, "----BEGIN SCRIPT ERROR----\n");
+    fprintf(stderr, "%s\n", error);
+    fprintf(stderr, "----END SCRIPT ERROR----\n");
 
     lua_close(L);
 }
