c01e276d29f609e2cd1dcd795c0c4f44fa047a83
Matthias Hannig <matthias@hannig.cc>
2018-05-03 20:28:44 +0200

    call render function with elapsed time

--- src/engine.c
+++ src/engine.c
@@ -1,6 +1,9 @@
 
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
 
 #include "engine.h"
 
@@ -36,11 +39,56 @@ einhorn_engine* engine_init(einhorn_config* config)
 }
 
 /*
+ * Helper: Get time delta
+ */
+double engine_get_timedelta(struct timeval t0, struct timeval t1)
+{
+    return (t1.tv_sec - t0.tv_sec) + (t1.tv_usec - t0.tv_usec) / 1000000.0f;
+}
+
+/*
+ * Call the lua render function
+ */
+int engine_call_render(einhorn_engine* engine, double t)
+{
+    // Push args on the stack
+    lua_getglobal(engine->L, "render");
+    lua_pushnumber(engine->L, 42);
+    lua_pushnumber(engine->L, t);
+
+    // Call render function with two arguments (fb and time)
+    int res = lua_pcall(engine->L, 2, 0, 0); 
+    if (res != 0) {
+        engine_lua_error(engine->L);
+        return -1;
+    }
+
+    // We don't expect it to return anything.
+    // Updates to the framebuffer are done in place.
+
+    return 0;
+}
+
+/*
  * Main rendering loop
  */
 int engine_run(einhorn_engine* engine)
 {
+    struct timeval t0;
+    struct timeval t1;
 
+    gettimeofday(&t0, NULL);
+
+    while(42) {
+        gettimeofday(&t1, NULL);
+        double t = engine_get_timedelta(t0, t1);
+        int ret = engine_call_render(engine, t); 
+        if (ret != 0) {
+            return -1;
+        }
+        usleep(1000000.0 * 1.0 / 60.0);
+    }
+      
     return 0;
 }
 
--- src/framebuffer.c
+++ src/framebuffer.c
@@ -1,9 +1,26 @@
 
 #include "framebuffer.h"
 
+/*
+ * Framebuffer datastructure layout
+ * 
+ * To keep things simple, the framebuffer is just an
+ * array of doubles with the length of: width * height + 2.
+ *
+ * This first two doubles are the width and height of the
+ * framebuffer.
+ *
+ * Convenience functions for access in lua are implemented.
+ */
+
 void framebuffer_init(lua_State* L)
 {
 
 
 }
+
+/*
+ * Lua Library Functions
+ */
+
 
